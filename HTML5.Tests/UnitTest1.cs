384bd349c4f330e942bd1166153da616330c2e0f
Steven Moberg <stevenmoberg@gmail.com>
2016-02-07 12:42:38 -0600

    New

---
+++ HTML5.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HTML5.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
